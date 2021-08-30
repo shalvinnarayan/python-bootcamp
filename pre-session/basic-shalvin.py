5ebbaea50c00509889e2a4cbf8a4c37beacf6436
Shalvin Narayan <shalvinnarayan@Shalvins-MacBook-Air.local>
2021-08-30 19:57:37 +1200

    Added basic-shalvin.py file

---
+++ pre-session/basic-shalvin.py
@@ -0,0 +1,23 @@
+#Created By: Shalvin Narayan
+#Email: shalvin.narayan@gmail.com
+#Date: 30 August 2021
+
+number = 6677
+
+number_decimal = 18.6
+
+height = 1.8
+
+name = 'Shalvin Narayan'
+
+print(number)
+
+print(number, ' is number')
+
+print(number_decimal, ' is decimal number')
+
+print(height, ' is height')
+
+print(name)
+
+print(name, ' is my name')
